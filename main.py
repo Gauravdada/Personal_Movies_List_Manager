adbd1eb8ef5d117b4d6215e7fa80ed3435dc1635
Gaurav Mazumder <gaurav.mazumder1997@gmail.com>
2020-04-02 17:05:34 +0000

    Finialy its Completed

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+
 movies = []
 
+"""
+movie = {
+    'name': ... (str),
+    'director': ... (str),
+    'year': ... (int)
+}
+"""
+
+
 def menu():
-    user_input = input("Enter 'a' to add movie, 'l' to see your movies, 'f' to find movie, 'q' to quit: ")
+    user_input = input("Enter 'a' to add a movie, 'l' to see your movies, 'f' to find a movie, and 'q' to quit: ")
 
     while user_input != 'q':
         if user_input == 'a':
@@ -11,49 +21,51 @@ def menu():
         elif user_input == 'f':
             find_movie()
         else:
-            print("Wrong input, try again")
+            print('Unknown command—please try again.')
 
-        user_input = input("Enter 'a' to add movie, 'l' to see your movies, 'f' to find movie, 'q' to quit: ")
+        user_input = input("\nEnter 'a' to add a movie, 'l' to see your movies, 'f' to find a movie, and 'q' to quit: ")
 
 
 def add_movie():
-    name = input("Enter movie name : ")
-    type = input("Enter type of movie : ")
-    rating = int(input("Enter your personal Rating : "))
+    name = input('Enter the movie name: ')
+    director = input('Enter the movie director: ')
+    year = input('Enter the movie release year: ')
 
     movies.append({
-        'Name' : name,
-        'Type' : type,
-        'Rating' : rating,
+        'name': name,
+        'director': director,
+        'year': year
     })
 
 
-def show_movies(movie_list):
-    for movie in movies:
-        show_movie_list(movie)
+def show_movies(movies_list):
+    for movie in movies_list:
+        show_movie_details(movie)
 
-        
-def show_movie_list(movie):
-    print(f"Name :- {movie['Name']} ")
-    print(f"Type :- {movie['Type']}")
-    print(f"Rating :- {movie['Rating']}")
-    print("")
+
+def show_movie_details(movie):
+    print(f"Name: {movie['name']}")
+    print(f"Director: {movie['director']}")
+    print(f"Release year: {movie['year']}")
 
 
 def find_movie():
-    find_by = input("What property of movie are you looking for : ")
-    looking_for = input("What are you searching for : ")
+    find_by = input("What property of the movie are you looking for? ")  # one of 'year', 'name', or 'director'
+    looking_for = input("What are you searching for? ")
 
-    found_movies = find_by_attributes(movies, looking_for, lambda x: x[find_by])
-    
-    
-def find_by_attributes(item, expected, finder)
+    found_movies = find_by_attribute(movies, looking_for, lambda x: x[find_by])
+
+    show_movies(found_movies)
+
+
+def find_by_attribute(items, expected, finder):
     found = []
 
     for i in items:
-      if finder(i) == expected:
-        found.append(i)
-  
+        if finder(i) == expected:
+            found.append(i)
+
     return found
+
 
 menu()
